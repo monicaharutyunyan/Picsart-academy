44871bc410379fd7c4f90377d0002ec3f83c8af3
Monica Harutyunyan <monikaharutyunyan003@gmail.com>
2024-07-05 17:04:09 +0400

    Some changes in repository

--- Homeworks/Homework_Jul_3/main.cpp
+++ Homeworks/Homework_Jul_3/main.cpp
@@ -2,15 +2,32 @@
 
 int main()
 {
-/*	Task1();
+	std::cout << "------------------ Task 1 ------------------" << std::endl;
+	Task1();
+	std::cout << std::endl;
+	std::cout << "------------------ Task 2 ------------------" << std::endl;
 	Task2();
+	std::cout << std::endl;
+	std::cout << "------------------ Task 3 ------------------" << std::endl;
 	Task3();
+	std::cout << std::endl;
+	std::cout << "------------------ Task 4 ------------------" << std::endl;
 	Task4();
+	std::cout << std::endl;
+	std::cout << "------------------ Task 5 ------------------" << std::endl;
 	Task5();
+	std::cout << std::endl;
+	std::cout << "------------------ Task 6 ------------------" << std::endl;
 	Task6();
+	std::cout << std::endl;
+	std::cout << "------------------ Task 7 ------------------" << std::endl;
 	Task7();
-*/	
+	std::cout << std::endl;
+	std::cout << "------------------ Task 8 ------------------" << std::endl;
 	Task8();
+	std::cout << std::endl;		
+
+
 	return 0;
 
 }
